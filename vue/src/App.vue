b7ec9c1ffb1de145e67dfc584a6351a7c45a2280
gonzacap <gonzalo.october@gmail.com>
2024-04-02 16:31:48 -0300

    update app vue

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,9 +1,17 @@
 <script setup>
+import { watch, ref } from 'vue';
+
 import PinBoard from './components/PinBoard.vue'
+
+import data from "./MockNotas.json";
+
+const noteList = ref(data);
+
 </script>
 
 <template>
   <main>
-    <PinBoard />
+    <h1>Notes PinBoard App</h1>
+    <PinBoard :noteList="noteList" />
   </main>
 </template>
