f347095a77a50e40b05809632adb61a0cd41f1d0
gonzacap <gonzalo.october@gmail.com>
2024-04-03 14:24:16 -0300

    add initial components

---
+++ vue/src/components/Note.vue
@@ -0,0 +1,13 @@
+<script setup>
+defineProps( ['title', 'description']);
+</script>
+
+<template>
+    <li>
+        <a href="" class="note-link">
+            <h2 class="aux">{{title}}</h2>
+            <p class="aux">{{description}}</p>
+        </a>
+    </li>
+</template>
+
--- vue/src/components/PinBoard.vue
+++ vue/src/components/PinBoard.vue
@@ -1,5 +1,21 @@
 <script setup>
+import { defineProps  } from 'vue';
+import Note from './Note.vue';
+
+const {
+    noteList
+} = defineProps(['noteList']);
+
 </script>
 
 <template>
+    <div class="npb_pinboard">
+        <ul>
+            <Note
+            v-for="(note, index) in noteList"
+            :title="note.title"
+            :description="note.description"
+            ></Note>
+        </ul>
+    </div>
 </template>
