f5186aaf27da23d3c33f17f7386bbf45c67dfad5
gonzacap <gonzalo.october@gmail.com>
2024-04-05 13:41:02 -0300

    add pin icon

--- vue/src/components/Note.vue
+++ vue/src/components/Note.vue
@@ -17,6 +17,9 @@ const sanitizedFooter = DOMPurify.sanitize(footer);
 <template>
     <li class="npb_note">
         <div class="npb_note_div">
+            <div class="npb_note_pin_div">
+                <img alt="Pin" src="./assets/icon-pin-down.svg" width="48" height="48" />
+            </div>
             <p class="npb_note_title">{{title}}</p>
             <div class="npb_note_description" v-if="sanitizedDescription" v-html="sanitizedDescription"></div>
             <template v-if="sanitizedFooter">
