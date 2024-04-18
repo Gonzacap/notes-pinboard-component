0b3e3619ca23e4364eb73c1d7f65f1b0fe391201
gonzacap <gonzalo.october@gmail.com>
2024-04-18 15:11:15 -0300

    scroll to center on mobile

--- vue/src/components/PinBoard.vue
+++ vue/src/components/PinBoard.vue
@@ -7,6 +7,13 @@ const {
     noteList
 } = defineProps(['noteList']);
 
+const centerOnFocus = (itemId) => {
+    const element = document.getElementById(`npb_note-${itemId}`);
+    if (element) {
+        element.scrollIntoView({ behavior: 'smooth', inline: "center" });
+    }
+};
+
 </script>
 
 <template>
@@ -14,10 +21,13 @@ const {
         <ul class="npb_pinboard_card_locations">
             <Note
             v-for="(note, index) in noteList"
+            :key="note?.id || index"
+            :id="`npb_note-${note?.id || index}`"
             :icon="note.icon"
             :title="note.title"
             :description="note.description"
             :footer="note.footer"
+            @click="() => centerOnFocus(note?.id || index)"
             ></Note>
         </ul>
     </div>
